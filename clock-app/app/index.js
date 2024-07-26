5b0034bdba9918e9a252d706486f2f77e3b57e4e
Dakuur <103331483+Dakuur@users.noreply.github.com>
2024-07-26 15:31:46 +0200

    Add files via upload

--- clock-app/app/index.js
+++ clock-app/app/index.js
@@ -1,6 +1,8 @@
 import clock from "clock";
 import * as document from "document";
 import { preferences } from "user-settings";
+import { HeartRateSensor } from "heart-rate";
+import { BodyPresenceSensor } from "body-presence";
 
 function zeroPad(i) {
   if (i < 10) {
@@ -26,11 +28,8 @@ clock.granularity = "minutes";
 // Initial data
 const backgroundImage = document.getElementById("backgroundImage");
 const labelHora = document.getElementById("labelHora");
+const labelBPM = document.getElementById("labelBPM");
 const foregroundImage = document.getElementById("foregroundImage");
-
-// Array of image paths (alternating between two images)
-const backPaths = ["fondobudi.png", "fondolujazo.png", "fondoferrari.png"];
-const forePaths = ["budi.png", "sasel.png", "ferrari.png"];
 
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
@@ -41,40 +40,26 @@ clock.ontick = (evt) => {
 
   let mins = zeroPad(today.getMinutes());
 
-  // Determina el índice para las imágenes basado en la hora
-  const hourIndex = today.getHours() % backPaths.length; // Esto dará un número entre 0 y 2
-
   // Cambia el fondo y la imagen de primer plano basado en la hora
   if (mins == 33) {
     // ALONSO
     backgroundImage.href = "fondoalonso.png";
-    foregroundImage.href = "alonso.png";
     labelHora.x = 168;
     labelHora.y = 148;
+    foregroundImage.href = "alonso.png";
   } else {
-    // Comportamiento normal basado en la hora
-    switch (hourIndex) {
-      case 0:
-        backgroundImage.href = backPaths[0];
-        foregroundImage.href = forePaths[0];
-        labelHora.style.fontSize = 120;
-        labelHora.x = 188;
-        labelHora.y = 148;
-        break;
-      case 1:
-        backgroundImage.href = backPaths[1];
-        foregroundImage.href = forePaths[1];
-        labelHora.style.fontSize = 110;
-        labelHora.x = 128;
-        labelHora.y = 148;
-        break;
-      case 2:
-        backgroundImage.href = backPaths[2];
-        foregroundImage.href = forePaths[2];
-        labelHora.x = 168;
-        labelHora.y = 253;
-        break;
-    }
+    backgroundImage.href = "fondoferrari.png";
+    labelHora.x = 168;
+    labelHora.y = 238;
+    foregroundImage.href = "ferrari.png";
+  }
+
+  if (HeartRateSensor) {
+    const hrm = new HeartRateSensor({ frequency: 1 });
+    hrm.addEventListener("reading", () => {
+      labelBPM.text = `${hrm.heartRate} km/h`;
+    });
+    hrm.start();
   }
 
   labelHora.text = `${hours}:${mins}`;
